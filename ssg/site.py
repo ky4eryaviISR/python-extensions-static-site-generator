1e093c74d31e243f49ee37b6ce7c3f6e0fe65e21
Vladimir <Vovchik.balagula@gmail.com>
2021-10-30 22:16:36 +0300

    module 4 commit
    Signed-off-by: Vladimir <Vovchik.balagula@gmail.com>

--- ssg/site.py
+++ ssg/site.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 from ssg import extensions
+from ssg import hooks
 
 class Site:
     def __init__(self, source, dest, parsers=None):
@@ -27,6 +28,7 @@ class Site:
             )
 
     def build(self):
+        hooks.event("collect_files", self.source, self.parsers)
         extensions.load_bundled()
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
